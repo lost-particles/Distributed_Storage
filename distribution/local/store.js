dd2a03601f0ed4e9189f8c7ac22f0015593cef20
subham <subhamkumardas852@gmail.com>
2024-03-10 23:54:40 -0400

    Update_1.2

--- distribution/local/store.js
+++ distribution/local/store.js
@@ -23,40 +23,79 @@ status.get(['nid'], (e, v)=>{
 
 const store = {
   dirPath: '../../store/'+nid+'/',
+  dirCreationPath: '../../store/'+nid,
   get: function(key, callback=(e, v)=>{}) {
-    fs.readFile(path.join(__dirname, this.dirPath+key+'.txt'), 'utf8',
-        (err, data) => {
-          if (err) {
-            callback(Error('File could not be found'), null);
-          } else {
-            callback(null, util.deserialize(data));
-          }
-        });
+    if (key === null) {
+      fs.readdir(path.join(__dirname, this.dirCreationPath), (err, files)=>{
+        if (err) {
+          callback(err, null);
+        } else {
+          const allKeys = [];
+          files.forEach((eachFile)=>{
+            allKeys.push(eachFile.toString().replace('.txt', ''));
+          });
+          callback(null, allKeys);
+          // const fileCount = files.length;
+          // let readCount =0;
+          // const allData = [];
+          // files.forEach((eachFile)=>{
+          //   fs.readFile(path.join(__dirname, this.dirPath+eachFile),
+          //       'utf8', (err, data) => {
+          //         readCount++;
+          //         if (err) {
+          //           allData.push(err);
+          //         } else {
+          //           allData.push(util.deserialize(data));
+          //         }
+          //         if (readCount===fileCount) {
+          //           callback(null, allData);
+          //         }
+          //       });
+          // });
+        }
+      });
+    } else {
+      fs.readFile(path.join(__dirname, this.dirPath+key+'.txt'), 'utf8',
+          (err, data) => {
+            if (err) {
+              callback(Error('File could not be found'), null);
+            } else {
+              callback(null, util.deserialize(data));
+            }
+          });
+    }
   },
   put: function(obj, key, callback=console.log) {
     if (key === null) {
       key = id.getID(obj);
     }
     const filePath = path.join(__dirname, this.dirPath+key+'.txt');
-    fs.mkdir(this.dirPath, {recursive: true}, (err) => {
-      // There could be issue down the line due to the
-      // use of flag { flag: 'wx' }
-      fs.writeFile(filePath,
-          util.serialize(obj), (err) => {
-            if (err) {
-              callback(err, null);
-            } else {
-              callback(null, obj);
-            }
-          });
-    });
+    fs.mkdir(path.join(__dirname, this.dirCreationPath), {recursive: true},
+        (err) => {
+          // There could be issue down the line due to the
+          // use of flag { flag: 'wx' }
+          fs.writeFile(filePath,
+              util.serialize(obj), (err) => {
+                if (err) {
+                  callback(err, null);
+                } else {
+                  callback(null, obj);
+                }
+              });
+        });
   },
   del: function(key, callback = console.log) {
-    fs.unlink(path.join(__dirname, this.dirPath+key), (err) => {
-      if (err) {
-        callback(err, null);
+    this.get(key, (e, v)=>{
+      if (e) {
+        callback(e, null);
       } else {
-        callback(null, 'Successfully deleted the file');
+        fs.unlink(path.join(__dirname, this.dirPath+key+'.txt'), (err) => {
+          if (err) {
+            callback(err, null);
+          } else {
+            callback(null, v);
+          }
+        });
       }
     });
   },
