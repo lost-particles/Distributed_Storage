f07e927d9d564b73ec28d6b7b64ebee6204a040f
subham <subhamkumardas852@gmail.com>
2024-03-12 23:25:22 -0400

    Update_1.6

--- distribution/all/store.js
+++ distribution/all/store.js
@@ -10,24 +10,43 @@ let store = (config) => {
   context.hash = config.hash || id.naiveHash;
   return {
     get: function(key, callback=(e, v)=>{}) {
-      localGroups.get(context.gid, (e, v)=>{
-        const nodeHashes = new Map();
-        const nids =[];
-        if (e && Object.keys(e).length !== 0) {
-          callback(e, null);
-        } else {
-          Object.keys(v).forEach((sid)=>{
-            nids.push(id.getID(v[sid]));
-            nodeHashes.set(id.getID(v[sid]), v[sid]);
+      if (key === null) {
+        const remote = {service: 'store', method: 'get'};
+        const message = [key];
+        distribution[context.gid].comm.send(message, remote, (e, v)=>{
+          // let aggregatedErrors = [];
+          let aggregatedKeys = [];
+          // Object.keys(e).forEach((ele)=>{
+          //   aggregatedErrors = [...aggregatedErrors, ...e[ele]];
+          // });
+          Object.keys(v).forEach((ele)=>{
+            aggregatedKeys = [...aggregatedKeys, ...v[ele]];
           });
-          targetNid = context.hash(id.getID(key), nids);
-          const remote = {node: nodeHashes.get(targetNid),
-            service: 'store', method: 'get'};
-          const message = [key];
-          localComm.send(message, remote, callback);
-        }
-      });
+          callback(e, aggregatedKeys);
+        });
+      } else {
+        localGroups.get(context.gid, (e, v)=>{
+          const nodeHashes = new Map();
+          const nids =[];
+          if (e && Object.keys(e).length !== 0) {
+            callback(e, null);
+          } else {
+            Object.keys(v).forEach((sid)=>{
+              nids.push(id.getID(v[sid]));
+              nodeHashes.set(id.getID(v[sid]), v[sid]);
+            });
+            targetNid = context.hash(id.getID(key), nids);
+            const remote = {node: nodeHashes.get(targetNid),
+              service: 'store', method: 'get'};
+            const message = [key];
+            localComm.send(message, remote, callback);
+          }
+        });
+      }
     }, put: (obj, key, callback = console.log) => {
+      if (key === null) {
+        key = id.getID(obj);
+      }
       localGroups.get(context.gid, (e, v)=>{
         console.log('Inside global groups'+ util.serialize(v));
         const nodeHashes = new Map();
